fe29d9e19117761bc7c79e6ae85427ad32db2a15
Alysson Silva <shirubaarison@gmail.com>
2024-03-06 19:24:11 -0300

    webSockets logic

---
+++ sockets/webSocketServer.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws')
+
+const connectedClients = new Set()
+
+const startWebSocketServer = () => {
+    const wss = new WebSocket.Server({ port: 3002 })
+
+    wss.on('connection', (ws) => {
+        console.log('new websocket connection')
+
+        connectedClients.add(ws)
+
+        ws.on('message', (data, isBinary) => {
+            const message = isBinary ? data : data.toString()
+            console.log('received ', message)
+            wss.clients.forEach(client => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(message)
+                }
+            })
+        })
+
+        ws.on('close', () => {
+            console.log('client disconnected')
+            connectedClients.delete(wss)
+        })
+    })
+
+    return wss
+}
+
+const getConnectedClients = () => {
+    return Array.from(connectedClients)
+}
+
+module.exports = { startWebSocketServer, getConnectedClients }
