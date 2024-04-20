bef8ec4e041d94d9bde600ab5a5267b4244a2d66
Alysson Silva <shirubaarison@gmail.com>
2024-04-20 15:52:07 -0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const config = require('./utils/config')
 const express = require('express')
 const connectDB = require('./connect')
+const usersRouter = require('./controllers/users')
 const mensagensRouter = require('./controllers/mensagens')
+const loginRouter = require('./controllers/login')
+
 
 const app = express()
 const cors = require('cors')
@@ -11,6 +14,8 @@ app.use(cors())
 app.use(express.json())
 connectDB()
 
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/mensagens', mensagensRouter)
 
 module.exports = app
